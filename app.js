7be0b6de64b887ae866b60d31880f4191a8a00a4
David Krier <dkflyboy@gmail.com>
2019-12-06 08:11:36 -0600

    updated app.js
    app.js updates

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello   jjjj from Appsody! This is Dave K ABC");
+  res.send("Hello -- from Appsody! This is Dave K ABC");
 });
 
 module.exports.app = app;
