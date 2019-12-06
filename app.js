6e7fc13d3e6c51b42c81dca19b2c24855c635183
David Krier <dkflyboy@gmail.com>
2019-12-06 08:17:02 -0600

    Friday

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello -- from Appsody! This is Dave K ABC");
+  res.send("Hello FRIDAY from Appsody! This is Dave K ABC");
 });
 
 module.exports.app = app;
